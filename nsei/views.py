24f435209e7dbb88d66a2fb2922d1db0786d31f9
Luiz Antonio <luizantoniossoares18@hotmail.com>
2023-02-05 11:11:34 -0300

    Adicionando as funcionabilidades de update e delete

--- nsei/views.py
+++ nsei/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa
 
 def home(request):
@@ -10,3 +10,19 @@ def salvar(request):
     Pessoa.objects.create(nome=vnome)
     pessoas = Pessoa.objects.all()
     return render(request, "index.html", {"pessoas": pessoas})
+
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, "update.html", {"pessoa": pessoa})
+
+def update(request, id):
+    vnome = request.POST.get("nome")
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = vnome
+    pessoa.save()
+    return redirect(home)
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect(home)
