3020cd56d3a78e1de77c69cb105486f4194f4265
Luiz Antonio <luizantoniossoares18@hotmail.com>
2023-02-11 20:48:42 -0300

    Criando as pastas Static e Images, como também modificando os settings e o index

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,10 @@ SECRET_KEY = 'django-insecure-n1khd%%0p=+$%wdf&fi&f7#8e-4%(6)q)ua#g$beoiba@-@jc)
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Application definition
